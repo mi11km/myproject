6bb15b114cd2bbc94701c93b52f611f2f89c0614
mi11km <mi11km.basket@gmail.com>
2020-04-25 23:06:44 +0900

    profile image func created

--- web_front/src/components/Search/ClubDetail.vue
+++ web_front/src/components/Search/ClubDetail.vue
@@ -1,47 +1,49 @@
 <template>
   <transition name="slide" mode="out-in" appear>
-    <v-img src="../../assets/top/top4.jpg">
-      <v-card class="pb-12 detail-transparent">
-        <v-breadcrumbs class="pb-0" :items="items">
-          <template v-slot:divider>
-            <v-icon>mdi-chevron-right</v-icon>
-          </template>
-        </v-breadcrumbs>
-        <v-carousel
-          cycle
-          height="400"
-          hide-delimiter-background
-          show-arrows-on-hover
-          class="mt-4 mb-4"
+    <v-card class="pb-12">
+      <v-carousel
+        cycle
+        hide-delimiter-background
+        show-arrows-on-hover
+        class="mb-4"
+        style="background-color: black"
+      >
+        <v-carousel-item
+          v-for="(item, i) in imageItems"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+          class="detail-content"
         >
-          <v-carousel-item v-for="i in 5" :key="i">
-            <v-sheet color="primary" height="100%">
-              <v-row class="fill-height" align="center" justify="center">
-                <div class="display-3">Slide{{ i }}</div>
-              </v-row>
-            </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
+        </v-carousel-item>
+      </v-carousel>
 
-        <v-card-title class="display-1 px-12">
-          {{ data.name }}
-        </v-card-title>
-        <v-card-subtitle class="px-12"
-          >{{ data.university }} --
-          {{ data.is_officially_approved }} ({{data.kind}})
-        </v-card-subtitle>
+      <v-card-title class="display-1 px-12 detail-content">
+        {{ data.name }}
+      </v-card-title>
+      <v-card-subtitle class="px-12 detail-content"
+        >{{ data.university }} -- {{ data.is_officially_approved }} ({{
+          data.kind
+        }})
+      </v-card-subtitle>
 
-        <v-card class="mx-auto detail elevation-0 pa-4 inner-transparent">
-          <v-card-title class="title-decoration mb-4">活動内容</v-card-title>
-          <v-card-text>{{ data.activity || this.template }}</v-card-text>
+      <v-card class="detail-content elevation-0 pa-4 inner-transparent">
+        <v-card-title class="title-decoration mb-4">活動内容</v-card-title>
+        <v-card-text>{{ data.activity || this.template }}</v-card-text>
 
-          <v-card-title class="title-decoration mb-4"
-            >みんなへのメッセージ！</v-card-title
-          >
-          <v-card-text>{{ data.message || this.template }}</v-card-text>
-        </v-card>
+        <v-card-title class="title-decoration mb-4"
+          >みんなへのメッセージ！</v-card-title
+        >
+        <v-card-text>{{ data.message || this.template }}</v-card-text>
       </v-card>
-    </v-img>
+
+      <v-breadcrumbs class="pb-0" :items="navItems">
+        <template v-slot:divider>
+          <v-icon>mdi-chevron-right</v-icon>
+        </template>
+      </v-breadcrumbs>
+    </v-card>
   </transition>
 </template>
 
@@ -53,17 +55,22 @@ export default {
   props: ["id"],
   data: () => ({
     data: {},
-    items: [
+    navItems: [
       { text: "サークル一覧", disabled: false, to: "/search" },
-      { text: "サークル詳細", disabled: false },
+      { text: "サークル詳細", disabled: false }
     ],
-    template: "まだ記入されてないよ！更新されるのを待ってね！",
+    template: "まだ記入されてないよ！更新されるのを待ってね！"
   }),
   created() {
-    api.get(`/clubs/${this.id}/`).then((response) => {
+    api.get(`/clubs/${this.id}/`).then(response => {
       this.data = response.data;
     });
   },
+  computed: {
+    imageItems() {
+      return [{ src: this.data.image }];
+    }
+  }
 };
 </script>
 
@@ -71,13 +78,13 @@ export default {
 .detail-transparent {
   background: rgba(255, 255, 255, 0.95) !important;
 }
-
-.detail {
-  max-width: 840px;
+.detail-content {
+  max-width: 1024px;
+  margin: 0 auto;
 }
 
 .inner-transparent {
-    background: rgba(255, 255, 255, 0.0) !important;
+  background: rgba(255, 255, 255, 0) !important;
 }
 
 .title-decoration {
--- web_front/src/store/modules/club.js
+++ web_front/src/store/modules/club.js
@@ -6,15 +6,15 @@ const authclub = {
   state: {
     choiceSelector: {
       kinds: ["運動系", "文化系", "技術系", "音楽系", "委員会系", "その他"],
-      approval: ["公認", "非公認"],
+      approval: ["公認", "非公認"]
     },
     authClubId: null,
-    clubData: null,
+    clubData: null
   },
   getters: {
-    choiceSelector: (state) => state.choiceSelector,
-    authClubId: (state) => state.authClubId,
-    clubData: (state) => state.clubData,
+    choiceSelector: state => state.choiceSelector,
+    authClubId: state => state.authClubId,
+    clubData: state => state.clubData
   },
   mutations: {
     set(state, clubData) {
@@ -23,30 +23,37 @@ const authclub = {
     },
     clear(state) {
       state.authClubId = null;
-    },
+    }
   },
   actions: {
     /**
      * サークル登録
      */
     register({ commit }, clubData) {
+      store.commit("message/clear");
+
+      const params = new FormData();
+      params.append("user", store.getters["auth/userId"]);
+      params.append("name", clubData.name);
+      params.append("university", clubData.university);
+      params.append("kind", clubData.kind);
+      params.append("is_officially_approved", clubData.is_officially_approved);
+      params.append("image", clubData.image);
+
       api
-        .post("/clubs/", {
-          user: store.getters["auth/userId"],
-          name: clubData.name,
-          university: clubData.university,
-          kind: clubData.kind,
-          is_officially_approved: clubData.is_officially_approved,
-        })
-        .then((response) => {
+        .post("/clubs/", params)
+        .then(response => {
           commit("set", response.data);
+        })
+        .catch(error => {
+          store.commit("message/setErrorMessage", error.response.data);
         });
     },
     /**
      * サークル情報取得(userのidから)
      */
     fetch({ commit }) {
-      api.get("/clubs/").then((response) => {
+      api.get("/clubs/").then(response => {
         const clubData = response.data;
         const user = store.getters["auth/userId"];
         for (let data of clubData) {
@@ -66,20 +73,20 @@ const authclub = {
         const clubId = getters.authClubId;
         api
           .patch(`/clubs/${clubId}/`, clubData)
-          .then((response) => {
+          .then(response => {
             console.log(response);
             dispatch("fetch");
             store.commit("message/setSuccessMessage", "変更しました");
             resolve();
           })
-          .catch((error) => {
+          .catch(error => {
             console.log(error);
             store.commit("message/setErrorMessage", error.response.data);
             reject();
           });
       });
-    },
-  },
+    }
+  }
 };
 
 export default authclub;
